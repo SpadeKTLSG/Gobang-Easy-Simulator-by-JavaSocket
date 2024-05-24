e9cf65353c909a146fd147f446c3d965e86b8e2c
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2024-05-24 17:50:37 +0800

    initial

---
+++ src/main/java/gobang/Main.java
@@ -0,0 +1,11 @@
+package gobang;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        log.info("Hello world!");
+    }
+}
